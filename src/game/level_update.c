88f682f68b8d09178689b51dfbbe51794395c483
Bitl <bitlblox@gmail.com>
2020-12-23 11:14:22 -0700

    simple way to fix repausing

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -209,7 +209,7 @@ u32 can_pause(void) {
     u32 val4 = get_dialog_id() >= 0;
     u32 intangible = (gMarioState->action & ACT_FLAG_INTANGIBLE) != 0;
 
-    if (!intangible && !val4 && !gWarpTransition.isActive && sDelayedWarpOp == WARP_OP_NONE && gCurrDemoInput == NULL) {
+    if (!intangible && !val4 && !gWarpTransition.isActive && sDelayedWarpOp == WARP_OP_NONE && sCurrPlayMode != PLAY_MODE_PAUSED && gCurrDemoInput == NULL) {
         return TRUE;
     }
 
